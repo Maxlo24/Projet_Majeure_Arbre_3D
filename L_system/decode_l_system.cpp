4cdc0bd942fabc717b9754f2edb2505ccbc0a163
julienchz <jchaize02@gmail.com>
2021-06-01 15:13:48 +0200

    decode_l_system

--- L_system/decode_l_system.cpp
+++ L_system/decode_l_system.cpp
@@ -1,6 +1,67 @@
 #include "decode_l_system.hpp"
 
+
+
+
+
 Decode_l_system::Decode_l_system()
 {
 
+}
+
+Decode_l_system::Decode_l_system(float alpha, float length)
+    :paramAlpha(alpha),paramLength(length)
+{}
+
+/*
+void Decode_l_system::incrementTree(L_system lSystem,node parentNode){
+    rules = lSystem.getOneRuleOfType(parentNode.Type());
+    for (lType type:rules.getRule()){
+        if (type == A){
+            node newN(A,paramAlpha,&parentNode);
+
+        }else if (type == B){
+
+        }else if (type == oG){
+
+        }else if (type == oD){
+
+        }else if (type == oPb){
+
+        }else if (type == oPh){
+
+        }else if (type == oRG){
+
+        }else if (type == oRD){
+
+        }else if (type == oT){
+
+        }else if (type == oSp){
+
+        }else if (type == oRp){
+
+        }
+
+
+    }
+}*/
+
+void Decode_l_system::setParamLength(float value)
+{
+    paramLength = value;
+}
+
+void Decode_l_system::setParamAlpha(float value)
+{
+    paramAlpha = value;
+}
+
+float Decode_l_system::getParamAlpha() const
+{
+    return paramAlpha;
+}
+
+float Decode_l_system::getParamLength() const
+{
+    return paramLength;
 }
--- L_system/decode_l_system.hpp
+++ L_system/decode_l_system.hpp
@@ -2,12 +2,26 @@
 #define DECODE_L_SYSTEM_HPP
 
 
+#include "../Tree/node.hpp"
+
+
 class Decode_l_system
 {
+private:
+    Rules rules;
+    vector<node*> nodes;
+    float paramAlpha;
+    float paramLength;
 public:
     Decode_l_system();
 
+    void incrementLSystem(L_system,node);
 
+
+    void setParamAlpha(float value);
+    void setParamLength(float value);
+    float getParamAlpha() const;
+    float getParamLength() const;
 };
 
 #endif // DECODE_L_SYSTEM_HPP
