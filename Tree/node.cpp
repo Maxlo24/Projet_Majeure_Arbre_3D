7366f1e463215a8e16b931665423151a6b1b85aa
Maxlo24 <max.gillot.69@gmail.com>
2021-06-01 12:16:29 +0200

    Update node

--- Tree/node.cpp
+++ Tree/node.cpp
@@ -3,5 +3,61 @@
 
 node::node()
 {
-
+    this->coord = vec2(0,0);
+    this->l_type = lType(A);
+    this->parent = nullptr;
 }
+
+node::node(vec2 coord)
+{
+    this->coord = coord;
+    this->l_type = lType(A);
+    this->parent = nullptr;
+}
+
+node::node(lType l_type, node* parent)
+{
+    this->coord = vec2(0,0);
+    this->l_type = l_type;
+    this->parent = parent;
+}
+
+node::node(vec2 coord,lType l_type, node *parent)
+{
+    this->coord = coord;
+    this->l_type = l_type;
+    this->parent = parent;
+}
+
+
+vec2 node::Coord() const
+{
+    return this->coord;
+}
+
+vec2& node::Coord()
+{
+    return this->coord;
+}
+
+
+lType node::Type() const
+{
+    return this->l_type;
+}
+
+lType& node::Type()
+{
+    return this->l_type;
+}
+
+const node* node::Parent() const
+{
+    return this->parent;
+}
+
+node*& node::Parent()
+{
+    return this->parent;
+}
+
--- Tree/node.hpp
+++ Tree/node.hpp
@@ -4,18 +4,31 @@
 #include<string>
 #include <iostream>
 #include "../vec2.cpp"
+#include "../L_system/l_system.h"
 
 class node
 {
 public:
     //constructor
     node();
-    node(vec2 coord)
+    node(vec2 coord);
+    node(lType l_type, node *parent);
+    node(vec2 coord,lType l_type, node *parent);
 
     //Methodes
+    vec2 Coord() const;
+    vec2& Coord();
+
+    lType Type() const;
+    lType& Type();
+
+    const node* Parent() const;
+    node*& Parent();
 
 private:
     vec2 coord;
+    lType l_type;
+    node *parent;
 
 };
 
